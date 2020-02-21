c695a00701ef4d8c80cae3c1df44d927a727ca22
DaniAkash <s.daniakash@gmail.com>
2020-02-21 06:56:58 +0530

    project created

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["tsc", "jest"],
+  root: true,
+  rules: {
+    quotes: ["warn", "double", { allowTemplateLiterals: true }],
+    "tsc/config": [
+      1,
+      {
+        configFile: "tsconfig.json"
+      }
+    ],
+    "comma-dangle": 0,
+    "react/prop-types": 1,
+    "prettier/prettier": 1,
+    "@typescript-eslint/no-unused-vars": 1
+  },
+  env: {
+    "jest/globals": true
+  }
+};
